d77f5851c2924b52b67dce311828a472547b6d94
Inhwan Lee <lih0905@gmail.com>
2020-07-18 00:04:02 +0900

    training model

---
+++ train.py
@@ -0,0 +1,66 @@
+# load modules
+import time 
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader 
+from transformers import AlbertModel
+
+from dataset import NSP_Dataset
+from model import NSP
+from utils import epoch_time, binary_accuracy, train, evaluate
+
+# fix random seed
+SEED = 1234
+
+random.seed(SEED)
+np.random.seed(SEED)
+torch.manual_seed(SEED)
+torch.backends.cudnn.deterministic = True
+
+# gpu 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# hyperparameters
+BATCH_SIZE = 32
+MAX_LENGTH = 512
+N_EPOCHS = 5
+
+# load the dataset
+train_dataset = NSP_Dataset('Data/train.csv', max_length=MAX_LENGTH)
+dev_dataset = NSP_Dataset('Data/dev.csv', max_length=MAX_LENGTH)
+train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE)
+dev_dataloader = DataLoader(dev_dataset, batch_size=BATCH_SIZE)
+
+# load the NSP model
+model_name='albert-base-v1'
+albert = AlbertModel.from_pretrained(model_name)
+model = NSP(albert)
+model.to(device)
+
+# load the optimizer and loss function
+optimizer = optim.Adam(model.parameters())
+criterion = nn.BCEWithLogitsLoss()
+criterion.to(device)
+
+# train
+best_valid_loss = float('inf')
+
+for epoch in range(N_EPOCHS):
+    start_time = time.time()
+    
+    train_loss, train_acc = train(model, train_dataloader, optimizer, criterion, device)
+    valid_loss, valid_acc = evaluate(model, valid_dataloader, criterion, device)
+    
+    end_time = time.time()
+
+    epoch_mins, epoch_secs = epoch_time(start_time, end_time)
+    
+    if valid_loss < best_valid_loss:
+        best_valid_loss = valid_loss
+        torch.save(model.state_dict(), 'nsp-model.pt')
+    
+    print(f'Epoch: {epoch+1:02} | Epoch Time: {epoch_mins}m {epoch_secs}s')
+    print(f'\tTrain Loss: {train_loss:.3f} | Train Acc: {train_acc*100:.2f}%')
+    print(f'\t Val. Loss: {valid_loss:.3f} |  Val. Acc: {valid_acc*100:.2f}%')
---
+++ utils.py
@@ -0,0 +1,75 @@
+
+import time
+
+import torch
+from tqdm import tqdm
+
+# calculate the elapsed time
+def epoch_time(start_time, end_time):
+    elapsed_time = end_time - start_time
+    elapsed_mins = int(elapsed_time / 60)
+    elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
+    return elapsed_mins, elapsed_secs
+
+# calculate accuracy
+def binary_accuracy(preds, y):
+    rounded_preds = torch.round(torch.sigmoid(preds))
+    correct = (rounded_preds==y).float()
+    acc = correct.sum() / len(correct)
+    return acc
+
+# train, evaluate functions
+def train(model, iterator, optimizer, criterion, device):
+    epoch_loss = 0
+    epoch_acc = 0
+    
+    model.train()
+    
+    for batch in tqdm(iterator):
+        optimizer.zero_grad()
+        input = batch[0].permute(1,0,2)
+        label = batch[1].type(torch.DoubleTensor).to(device)
+        
+        input_ids = input[0].to(device)
+        attention_mask = input[2].to(device)
+        token_type_ids = input[1].to(device)
+        
+        data = (input_ids, attention_mask, token_type_ids)
+        
+        predictions = model(data).squeeze(1) # output_dim = 1
+        loss = criterion(predictions, label)
+        acc = binary_accuracy(predictions, label)
+        
+        loss.backward()
+        optimizer.step()
+        
+        epoch_loss += loss.item()
+        epoch_acc += acc.item()
+        
+    return epoch_loss / len(iterator), epoch_acc / len(iterator)
+
+def evaluate(model, iterator, criterion, device):
+    epoch_loss = 0
+    epoch_acc = 0
+    
+    model.eval()
+    
+    with torch.no_grad():
+        for batch in tqdm(iterator):
+            input = batch[0].permute(1,0,2)
+            label = batch[1].type(torch.DoubleTensor).to(device)
+
+            input_ids = input[0].to(device)
+            attention_mask = input[2].to(device)
+            token_type_ids = input[1].to(device)
+
+            data = (input_ids, attention_mask, token_type_ids)
+
+            predictions = model(data).squeeze(1) 
+            loss = criterion(predictions, label)
+            acc = binary_accuracy(predictions, label)
+
+            epoch_loss += loss.item()
+            epoch_acc += acc.item()
+        
+    return epoch_loss / len(iterator), epoch_acc / len(iterator)
