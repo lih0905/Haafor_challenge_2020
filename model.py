e486b3aebe1b65c7d83fb71a031a3295bbc20d4d
Inhwan Lee <lih0905@lxper.com>
2020-07-31 12:03:44 +0000

    Trained albert-large-v1 NSP_gru with AdamP

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch import sigmoid
 from transformers import AlbertTokenizer, AlbertModel
 
@@ -62,12 +63,12 @@ class NSP_gru(nn.Module):
         embedding_dim = albert.config.to_dict()['hidden_size']
         hidden_dim = albert.config.to_dict()['hidden_size']
         self.rnn = nn.GRU(embedding_dim, hidden_dim,
-                    num_layers = 2,
+                    num_layers = 4,
                     bidirectional = True,
                     batch_first = True,
-                    dropout = 0.1)
+                    dropout = 0.3)
         self.out = nn.Linear(2*hidden_dim, 1)
-        self.dropout = nn.Dropout(0.1)
+        self.dropout = nn.Dropout(0.3)
         
     def forward(self, input):
         # input = (input_ids, attention_mask, token_type_ids)
@@ -84,3 +85,74 @@ class NSP_gru(nn.Module):
 
         out = self.out(hidden)
         return out
+
+class NSP_gru_NoneFreeze(nn.Module):
+    def __init__(self, albert):
+        super().__init__()
+        self.albert = albert
+        embedding_dim = albert.config.to_dict()['hidden_size']
+        hidden_dim = albert.config.to_dict()['hidden_size']
+        self.rnn = nn.GRU(embedding_dim, hidden_dim,
+                    num_layers = 4,
+                    bidirectional = True,
+                    batch_first = True,
+                    dropout = 0.3)
+        self.out = nn.Linear(2*hidden_dim, 1)
+        self.dropout = nn.Dropout(0.3)
+        
+    def forward(self, input):
+        # input = (input_ids, attention_mask, token_type_ids)
+        embedded = self.albert(*input)[0]
+        # embedded = (batch_size, sentence_length, embedding_dim)
+
+        _, hidden = self.rnn(embedded)
+        # hidden = (num_layers * 2, batch_size, hidden_dim)
+
+        hidden = self.dropout(torch.cat((hidden[-2,:,:], hidden[-1,:,:]), dim=1))
+        # hidden = (batch_size, 2*hidden_dim)
+
+
+        out = self.out(hidden)
+        return out
+
+
+class CNN(nn.Module):
+    def __init__(self, albert, n_filters, filter_sizes, dropout):
+        super().__init__()
+        self.albert = albert
+        embedding_dim = albert.config.to_dict()['hidden_size']
+        hidden_dim = albert.config.to_dict()['hidden_size']
+        self.convs = nn.ModuleList([nn.Conv2d(in_channels=1,
+                                             out_channels=n_filters,
+                                             kernel_size=(fs, embedding_dim))
+                                   for fs in filter_sizes])
+        self.fc = nn.Linear(len(filter_sizes) * n_filters, 1)
+        self.dropout = nn.Dropout(dropout)
+        
+    def forward(self, input):
+        # input = (input_ids, attention_mask, token_type_ids)
+        with torch.no_grad():
+            embedded = self.albert(*input)[0]
+        # embedded = (batch_size, sentence_length, embedding_dim)
+        
+        embedded = embedded.unsqueeze(1)
+        #print_shape('embedded', embedded)
+        # embedded = [batch_size, 1, sent_len, emb_dim]
+        
+        #print_shape('self.convs[0](embedded)', self.convs[0](embedded))
+        # self.convs[0](embedded) = [batch_size, n_filters, sent_len-filter_sizes[n]+1, 1 ]
+        conved = [F.relu(conv(embedded)).squeeze(3) for conv in self.convs]
+        
+        #print_shape('F.max_pool1d(conved[0], conved[0].shape[2])', F.max_pool1d(conved[0], conved[0].shape[2]))
+        # F.max_pool1d(conved[0], conved[0].shape[2]) = [batch_size, n_filters, 1]
+        pooled = [F.max_pool1d(conv, conv.shape[2]).squeeze(2) for conv in conved]
+        
+        cat = self.dropout(torch.cat(pooled, dim=1))
+        #print_shape('cat', cat)
+        # cat = [batch_size, n_filters * len(filter_size)]
+        
+        res = self.fc(cat)
+        #print_shape('res', res)
+        # res = [batch_size, output_dim]
+        
+        return self.fc(cat)
--- train.py
+++ train.py
@@ -8,10 +8,11 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader 
-from transformers import AlbertModel
+from transformers import AlbertModel, BertModel, AutoModel
+from adamp import AdamP
 
 from dataset import NSP_Dataset
-from model import NSP, NSP_pool, NSP_lin, NSP_gru
+from model import NSP, NSP_pool, NSP_lin, NSP_gru, NSP_gru_NoneFreeze, CNN
 from utils import epoch_time, binary_accuracy, train, evaluate, send_telegram_message
 
 # fix random seed
@@ -28,8 +29,9 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # hyperparameters
 BATCH_SIZE = 128
 MAX_LENGTH = 512
-N_EPOCHS = 10
+N_EPOCHS = 20
 model_name='albert-large-v1'
+#model_name= 'roberta-base'
 
 # load the dataset
 train_dataset = NSP_Dataset('Data/train.csv', model_name=model_name, max_length=MAX_LENGTH)
@@ -38,25 +40,29 @@ train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, num_workers=
 dev_dataloader = DataLoader(dev_dataset, batch_size=BATCH_SIZE, num_workers=4)
 
 # load the NSP model
-albert = AlbertModel.from_pretrained(model_name)
-model = NSP_gru(albert)
+# albert = AlbertModel.from_pretrained(model_name)
+bert = AutoModel.from_pretrained(model_name)
+# model = NSP_gru(bert)
+N_FILTERS = 100
+FILTER_SIZES = [3,4,5]
+model = CNN(bert, N_FILTERS, FILTER_SIZES, 0.5)
 MODEL = str(model.__class__).split('.')[1].split("'")[0]
-# model_path = f'weights/{MODEL}_{model_name}_batch_{BATCH_SIZE}_epoch_{1}.pt'
+# model_path = f'weights/{MODEL}_{model_name}_batch_{BATCH_SIZE}_epoch_{6}.pt'
 # model.load_state_dict(torch.load(model_path))
 model.to(device)
 
-
 # freeze the parameters of albert
 for name, param in model.named_parameters():
-    if name.startswith('albert'):
+    if name.startswith('albert') or name.startswith('bert'):
         param.requires_grad = False
 
 # load the optimizer and loss function
 optimizer = optim.Adam(model.parameters())
+# optimizer = AdamP(model.parameters(), lr=0.001, betas=(0.9, 0.999), weight_decay=1e-2)
 criterion = nn.BCEWithLogitsLoss()
 criterion.to(device)
 
-# train
+train
 try:
     with open(f'weights/{MODEL}_{model_name}_best_loss.pickle','rb') as f:
         last_epoch, best_valid_loss = pickle.load(f)
@@ -64,8 +70,12 @@ except:
     last_epoch = 0
     best_valid_loss = float('infinity')
 
+# last_epoch = 7
+# best_valid_loss = 0.596
+
+
 for epoch in range(last_epoch, N_EPOCHS):
-    print(f'Train the {epoch}th epoch.')
+    print(f'Train the {epoch+1}th epoch.')
     print(f'The current best loss is {best_valid_loss:.3f}.')
     start_time = time.time()
     
@@ -78,15 +88,17 @@ for epoch in range(last_epoch, N_EPOCHS):
     
     if valid_loss < best_valid_loss:
         best_valid_loss = valid_loss
-        # torch.save(model.state_dict(), f'weights/{MODEL}_{model_name}_batch_{BATCH_SIZE}_epoch_{epoch}.pt')        
-        # with open(f'weights/{MODEL}_{model_name}_best_loss.pickle', 'wb') as f:
-        #     pickle.dump([epoch+last_epoch+1, best_valid_loss], f)
-
+        torch.save(model.state_dict(), f'weights/{MODEL}_{model_name}_batch_{BATCH_SIZE}_epoch_{epoch}.pt')        
+        with open(f'weights/{MODEL}_{model_name}_best_loss.pickle', 'wb') as f:
+            pickle.dump([epoch+1, best_valid_loss], f)
+        
     msg = f'Model : {MODEL}_{model_name}\n' + \
-        f'After training {epoch+last_epoch+1} epochs of {N_EPOCHS} epochs, valid loss = {valid_loss:.3f} and valid acc. = {valid_acc*100:.2f}.\n' + \
-        f'Current best valid loss. = {best_valid_loss:.3f}'
+        f'After training {epoch+1} epochs of {N_EPOCHS} epochs, valid loss = {valid_loss:.3f} and valid acc. = {valid_acc*100:.2f}.\n' + \
+        f'Current best valid loss. = {best_valid_loss:.3f}\n'
     send_telegram_message(msg)
+    with open(f'result.txt', 'a') as f:
+        f.write(msg)
 
-    print(f'Epoch: {epoch+last_epoch+1:02} | Epoch Time: {epoch_mins}m {epoch_secs}s')
+    print(f'Epoch: {epoch+1:02} | Epoch Time: {epoch_mins}m {epoch_secs}s')
     print(f'\tTrain Loss: {train_loss:.3f} | Train Acc: {train_acc*100:.2f}%')
     print(f'\t Val. Loss: {valid_loss:.3f} |  Val. Acc: {valid_acc*100:.2f}%')
